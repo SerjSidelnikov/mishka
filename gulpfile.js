991e315b692a172dca036fe7049bf69431adba0b
Серж <serj2107@gmail.com>
2017-04-14 20:32:03 +0300

    Сделал сетку для блока header

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,39 @@
 'use strict';
 
 const gulp = require('gulp');
-const newer = require('gulp-newer');
+const less = require('gulp-less');
+const plumber = require('gulp-plumber');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const mqpacker = require('css-mqpacker');
+const minify = require('gulp-csso');
+const rename = require('gulp-rename');
 const del = require('del');
 const run = require('run-sequence');
 const server = require('browser-sync').create();
 
-gulp.task('assets', function () {
+gulp.task('style', function () {
+  gulp.src('frontend/less/style.less')
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(postcss([
+      mqpacker({sort: true}),
+      autoprefixer(['last 2 versions'])
+    ]))
+    .pipe(gulp.dest('build/css'))
+    .pipe(minify())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css'))
+    .pipe(server.stream());
+});
+
+gulp.task('copy', function () {
   return gulp.src([
     'frontend/fonts/**/*.{woff,woff2}',
     'frontend/img/**',
     'frontend/js/**',
     'frontend/*.html'
   ], {base: 'frontend'})
-    .pipe(newer('build'))
     .pipe(gulp.dest('build'));
 });
 
@@ -27,9 +47,20 @@ gulp.task('clean', function () {
 gulp.task('build',  function (fn) {
   run(
     'clean',
-    'assets',
+    'copy',
+    'style',
     fn
   );
+});
+
+gulp.task('html:copy', function () {
+  return gulp.src(['frontend/*.html', 'frontend/img/**'])
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('html:update', ['html:copy'], function (fn) {
+  server.reload();
+  fn();
 });
 
 gulp.task('serve', function () {
@@ -37,6 +68,6 @@ gulp.task('serve', function () {
     server: 'build'
   });
 
-  gulp.watch('frontend/**', ['assets']);
-  server.watch('build/**/*.*').on('change', server.reload);
+  gulp.watch('frontend/**', ['style']);
+  gulp.watch('frontend/*.html', ['html:update']);
 });
